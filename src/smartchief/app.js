83fc02c85cec06ebf36c5f3867cbb678ef57b75c
grudengroup\zhen.yuan <zhen.yuan@gruden.com>
2018-06-30 00:53:43 +1000

    complete task 1

--- src/smartchief/app.js
+++ src/smartchief/app.js
@@ -1,16 +1,31 @@
+'use strict';
+
 const express = require('express')
 const app = express();
 const obj = require("./getObjectsFromImg");
 
 
+//TODO: Get food from image via azure cognitive service.
+app.get('/', (req, res) => {
+  obj.getFoods(req.query.img, res);
+})
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.post('/', function (req, res) {
+//   var data = obj.getFoods();
+//   res.send(data);
+// })
 
-app.post('/', function (req, res) {
-    var data = obj.getFoods();
-    res.send(data);
-  })
+//starts application.
+app.listen(3000, () => {
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+  var fs = require('fs');
+
+  fs.readFile('logo.txt', 'utf8', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+  });
+
+  console.log('APP listening on port 3000!')
+})
 
 
--- src/smartchief/getObjectsFromImg.js
+++ src/smartchief/getObjectsFromImg.js
@@ -1,8 +1,33 @@
-exports.getFoods = function(req, res) {
-    var data ={
-        type: "fruit",
-        number: 10
+const request = require('request');
+const subscriptionKey = 'fe78359472384122a8b8f32ab47d5dac';
+const uriBase = 'https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze';
+
+exports.getFoods = function(img, res) {
+
+    const imageUrl = img;
+    const params = {
+        'visualFeatures': 'Categories,Description,Color,Tags',
+        'details': '',
+        'language': 'en'
     };
 
-   return data;
+    const options = {
+        uri: uriBase,
+        qs: params,
+        body: '{"url": ' + '"' + imageUrl + '"}',
+        headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': subscriptionKey
+        }
+    };
+   
+    request.post(options, (error, response, body) => {
+        if (error) {
+            console.log('Error: ', error);
+            return;
+        }
+        let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+
+        res.send(jsonResponse);
+    });
 };
