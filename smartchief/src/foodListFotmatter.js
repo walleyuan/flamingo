659d3c95b1103ee321801dd286b116ea0620d86c
Matilda Pan <matilda@flamingo.com>
2018-06-30 13:34:51 +1000

    minor change on foodlist

--- smartchief/src/foodListFotmatter.js
+++ smartchief/src/foodListFotmatter.js
@@ -1,13 +1,12 @@
 const foodListFormatter = (rawData) => {
-  console.log("RAWDATA%%", rawData);
   const foodList = {};
   rawData.forEach((element)=>{
     const foodName = element.tagName;
     if(!foodList[foodName]){
       foodList[foodName] = 1;
+    }else{
+      foodList[foodName] += 1;
     }
-    const originalNumb = foodList[foodName];
-    foodList[foodName] = originalNumb + 1;
   });
   return foodList;
 }
